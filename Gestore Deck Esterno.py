a6fc17714e9df7155cc6df0f6f4678e5c2c4141d
Gabriele Fordini <48223879+ColdWaters@users.noreply.github.com>
2020-09-16 01:29:36 +0200

    Aggiunto gestore file multiplo
    L'utente ora può selezionare molteplici file tramite una finestra di dialogo

--- Gestore Deck Esterno.py
+++ Gestore Deck Esterno.py
@@ -1,6 +1,16 @@
-import csv, json, glob
+import csv, json
 
-def editor(row_local):
+import tkinter as tk
+from tkinter import filedialog
+
+def OnlyFilenames(imput): #la funzione OnlyFilenames prende in imput i percorsi dei diversi file, e ritorna una lista con solo i namefile
+    place = []
+    for vac in imput:
+        vac = vac.split("/")
+        place.append(vac[-1])
+    return place
+
+def editor(row_local): #la funzione editor permette di indentare i valori cards__effects__influence e cards__effects__wealth nel file JSON
     temp = {}
     temp["cards__effects__influence"] = row_local.get("cards__effects__influence")
     temp["cards__effects__wealth"] = row_local.get("cards__effects__wealth")
@@ -15,31 +25,31 @@ def GetNameFaction(imput_gnf):
     return temp[0].lower()
 
 #assegna alla variabile poth_in il nome del file .csv
-#se sono presenti più file CSV nello stesso percoso, il programma prende in considerazione il primo in ordine alfabetico
-path_in = glob.glob('*.csv')[0]
+#il programma prende in imput una lista di file selezionati dall'utente, tramite una finestra di dialogo
+root = tk.Tk()
+root.withdraw()
 
-#prende la fazione dal nome del file, eliminando estensioni di file e altre porcherie. nota: tutti i file .csv devono essere nomitati secondo la loro fazione.
-faction = GetNameFaction(path_in)
-jsonFilePath = f"{faction}.js"
+file_path = filedialog.askopenfilenames()
 
+for path_in in OnlyFilenames(file_path):
 
+    #prende la fazione dal nome del file, eliminando estensioni di file e altre porcherie. nota: tutti i file .csv devono essere nomitati secondo la loro fazione.
+    faction = GetNameFaction(path_in)
+    jsonFilePath = f"{faction}.js"
 
-#il dizionario data_out viene usato per assemblare i dati prima di essere trasferiti in JSON
-data_out = {}
+    #il dizionario data_out viene usato per assemblare i dati prima di essere trasferiti in JSON
+    data_out = {}
 
-#legge il file CSV
-with open(path_in) as file_csv:
-    #la funzione DictReader legge il file CSV colonna per colonna
-    reader = csv.DictReader(file_csv)
+    #legge il file CSV
+    with open(path_in) as file_csv:
+        #la funzione DictReader legge il file CSV colonna per colonna
+        reader = csv.DictReader(file_csv)
 
-    rows = list(reader)
+        rows = list(reader)
 
-    for counter in range(len(rows)):
-        editor(rows[counter])
+        for counter in range(len(rows)):
+            editor(rows[counter])
+            data_out[faction] = rows
 
-
-    data_out[faction] = rows
-
-
-with open(jsonFilePath, "w") as f:
-    json.dump(data_out, f, indent=4)
+    with open(jsonFilePath, "w") as f:
+        json.dump(data_out, f, indent=4)
